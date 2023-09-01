ceed8b6362292b3999b5b2573933374c317c3c4a
abhayychaudharyy <112614245+abhayychaudharyy@users.noreply.github.com>
2023-09-01 09:50:23 +0530

    Add files via upload

---
+++ MyCalc.java
@@ -0,0 +1,16 @@
+package exp2;
+
+public class MyCalc {
+	public int sum(int a, int b) {
+		return(a+b);
+	}
+	public int diff(int a, int b) {
+		return(a-b);
+	}
+	public static void main(String[] args) {
+		MyCalc calc= new MyCalc();
+		System.out.println("Sum is"+calc.sum(4,6));
+		System.out.println("Diff is"+calc.diff(10,4));
+	}
+
+}
