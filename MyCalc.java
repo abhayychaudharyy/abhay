57267ad8efb8832f5a35be50fb5b6a33ecec6477
abhayychaudharyy <abhayjan201@gmail.com>
2023-09-07 08:12:42 +0530

    added functions

--- MyCalc.java
+++ MyCalc.java
@@ -7,10 +7,18 @@ public class MyCalc {
 	public int diff(int a, int b) {
 		return(a-b);
 	}
+	public int multi(int a, int b) {
+		return(a*b);
+	}
+	public int div(int a, int b) {
+		return(a/b);
+	}
 	public static void main(String[] args) {
 		MyCalc calc= new MyCalc();
 		System.out.println("Sum is"+calc.sum(4,6));
 		System.out.println("Diff is"+calc.diff(10,4));
+		System.out.println("Multi is"+calc.multi(4,6));
+		System.out.println("Div is"+calc.div(10,4));
 	}
 
 }
